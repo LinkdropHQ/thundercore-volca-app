17d51a7d37dcdc2d4521db3d067d43c4502aef97
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2019-04-12 11:34:21 +0200

    wrong network updated for thunder core

--- src/components/NotConnectedScreens/UnsupportedNetwork.jsx
+++ src/components/NotConnectedScreens/UnsupportedNetwork.jsx
@@ -89,12 +89,12 @@ class UnsupportedNetwork extends Component {
         return (
             <div>
                 <div style={styles.title}>Network is not supported</div>
-                <div style={{ ...styles.instructionsText, textAlign: 'center' }}> We support Ethereum Main<br />
-                    and Ropsten Test Networks </div>
+                <div style={{ ...styles.instructionsText, textAlign: 'center' }}> You should be connected to ThunderCore Network<br />
+                </div>
                 <div style={styles.instructionsContainer}>
                     <div style={{ ...styles.instructionsText, fontFamily: 'SF Display Bold' }}>How to change Network:</div>
                     <div style={styles.instructionsText}> 1. Go to Settings in your wallet app</div>
-                    <div style={styles.instructionsText}> 2. Swich Network to <div style={styles.instructionsTextBold}>Ethereum Main</div> </div>
+                    <div style={styles.instructionsText}> 2. Swich Network to <div style={styles.instructionsTextBold}>ThunderCore</div> </div>
                     <div style={styles.instructionsText}> 3. Back to DApp browser in your wallet and reload the receiver’s link </div>
                 </div>
             </div>
